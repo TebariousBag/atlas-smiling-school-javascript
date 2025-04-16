834a9e5003fe0a58e06ee7e87e4a6f5a14b4085b
“Tristian <tristian.davis@atlasschool.com>
2025-04-16 12:01:38 -0500

    fixed quotes on pricing page

--- scripts.js
+++ scripts.js
@@ -162,3 +162,51 @@ $(document).ready(function () {
       }
     });
   });
+
+// PRICING PAGE QUOTES
+$(document).ready(function () {
+    const $quotesCarouselInner = $('#quotesCarousel .carousel-inner');
+    const $quotesLoader = $('.quotes .loader');
+  
+    $.ajax({
+      url: 'https://smileschool-api.hbtn.info/quotes',
+      method: 'GET',
+      beforeSend: function () {
+        $quotesLoader.show();
+      },
+      success: function (data) {
+        $quotesLoader.hide();
+        $quotesCarouselInner.empty();
+  
+        data.forEach((quote, index) => {
+          const activeClass = index === 0 ? 'active' : '';
+          const quoteItem = `
+            <div class="carousel-item ${activeClass}">
+              <div class="row mx-auto align-items-center">
+                <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
+                  <img src="${quote.pic_url}" class="d-block align-self-center rounded-circle" alt="${quote.name}">
+                </div>
+                <div class="col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0">
+                  <div class="quote-text">
+                    <p class="text-white">« ${quote.text} »</p>
+                    <h4 class="text-white font-weight-bold">${quote.name}</h4>
+                    <span class="text-white">${quote.title}</span>
+                  </div>
+                </div>
+              </div>
+            </div>`;
+          $quotesCarouselInner.append(quoteItem);
+        });
+  
+        $('#quotesCarousel').carousel({
+          interval: false,
+          ride: false
+        });
+      },
+      error: function () {
+        $quotesLoader.hide();
+        $quotesCarouselInner.append('<div class="text-danger">Failed to load quotes</div>');
+      }
+    });
+  });
+  
