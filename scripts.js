be8d827d24addd3e9a8aa7dd053a071cb1112077
“Tristian <tristian.davis@atlasschool.com>
2025-04-16 12:29:55 -0500

    i am not a fan of carousels

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
       success: function (data) {
         $quotesLoader.hide();
         $quotesCarouselInner.empty();
-  
+
         data.forEach((quote, index) => {
           const activeClass = index === 0 ? 'active' : '';
           const quoteItem = `
@@ -32,13 +32,19 @@ $(document).ready(function () {
             </div>`;
           $quotesCarouselInner.append(quoteItem);
         });
+
+        // Re-initialize carousel after loading content
+        $('#quotesCarousel').carousel({
+          interval: false,
+          ride: false
+        });
       },
       error: function () {
         $quotesLoader.hide();
         $quotesCarouselInner.append('<div class="text-danger">Failed to load quotes</div>');
       }
     });
-  
+
     // POPULAR TUTORIALS CAROUSEL
     const $tutorialsCarouselInner = $('#carouselTutorials');
     const $tutorialLoader = $('.popular-tutorials .loader');
@@ -52,10 +58,10 @@ $(document).ready(function () {
       success: function (data) {
         $tutorialLoader.hide();
         $tutorialsCarouselInner.empty();
-  
+
         for (let i = 0; i < data.length; i += 4) {
           const activeClass = i === 0 ? 'active' : '';
-  
+
           let cards = '';
           for (let j = i; j < i + 4 && j < data.length; j++) {
             const tutorial = data[j];
@@ -78,17 +84,18 @@ $(document).ready(function () {
                 </div>
               </div>`;
           }
-  
+
           const slide = `
             <div class="carousel-item ${activeClass}">
               <div class="row">
                 ${cards}
               </div>
             </div>`;
-  
+
           $tutorialsCarouselInner.append(slide);
         }
-  
+
+        // Re-initialize carousel after loading content
         $('#tutorialCarousel').carousel({
           interval: false,
           ride: false
@@ -99,7 +106,7 @@ $(document).ready(function () {
         $tutorialsCarouselInner.append('<div class="text-danger">Failed to load tutorials</div>');
       }
     });
-  
+
     // LATEST VIDEOS CAROUSEL
     const $latestVideosCarouselInner = $('#latestVideosCarousel .carousel-inner');
     const $latestVideosLoader = $('.latest-videos .loader');
@@ -116,7 +123,7 @@ $(document).ready(function () {
         // Loop through the data and create carousel items
         for (let i = 0; i < data.length; i += 4) {
           const activeClass = i === 0 ? 'active' : '';
-  
+
           let cards = '';
           for (let j = i; j < i + 4 && j < data.length; j++) {
             const video = data[j];
@@ -147,10 +154,11 @@ $(document).ready(function () {
                 ${cards}
               </div>
             </div>`;
-  
+
           $latestVideosCarouselInner.append(slide);
         }
-  
+
+        // Re-initialize carousel after loading content
         $('#latestVideosCarousel').carousel({
           interval: false,
           ride: false
@@ -161,52 +169,4 @@ $(document).ready(function () {
         $latestVideosCarouselInner.append('<div class="text-danger">Failed to load latest videos</div>');
       }
     });
-  });
-
-// PRICING PAGE QUOTES
-$(document).ready(function () {
-    const $quotesCarouselInner = $('#quotesCarousel .carousel-inner');
-    const $quotesLoader = $('.quotes .loader');
-  
-    $.ajax({
-      url: 'https://smileschool-api.hbtn.info/quotes',
-      method: 'GET',
-      beforeSend: function () {
-        $quotesLoader.show();
-      },
-      success: function (data) {
-        $quotesLoader.hide();
-        $quotesCarouselInner.empty();
-  
-        data.forEach((quote, index) => {
-          const activeClass = index === 0 ? 'active' : '';
-          const quoteItem = `
-            <div class="carousel-item ${activeClass}">
-              <div class="row mx-auto align-items-center">
-                <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
-                  <img src="${quote.pic_url}" class="d-block align-self-center rounded-circle" alt="${quote.name}">
-                </div>
-                <div class="col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0">
-                  <div class="quote-text">
-                    <p class="text-white">« ${quote.text} »</p>
-                    <h4 class="text-white font-weight-bold">${quote.name}</h4>
-                    <span class="text-white">${quote.title}</span>
-                  </div>
-                </div>
-              </div>
-            </div>`;
-          $quotesCarouselInner.append(quoteItem);
-        });
-  
-        $('#quotesCarousel').carousel({
-          interval: false,
-          ride: false
-        });
-      },
-      error: function () {
-        $quotesLoader.hide();
-        $quotesCarouselInner.append('<div class="text-danger">Failed to load quotes</div>');
-      }
-    });
-  });
-  
+});
